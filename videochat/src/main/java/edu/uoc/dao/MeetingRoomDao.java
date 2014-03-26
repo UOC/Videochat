515dbce006c8b72325440d65d8933842263312f3
Francesc Fernandez <francesc@tresipunt.com>
2014-03-26 12:54:20 +0100

    MeetingRoomDao partially implemented

---
+++ videochat/src/main/java/edu/uoc/dao/MeetingRoomDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.uoc.dao;
+
+import edu.uoc.model.MeetingRoom;
+
+/**
+ *
+ * @author Francesc Fernandez
+ */
+public interface MeetingRoomDao {
+    
+    void save(MeetingRoom meetingRoom);
+    
+    void update(MeetingRoom meetingRoom);
+    
+    void delete(MeetingRoom meetingRoom);
+}
