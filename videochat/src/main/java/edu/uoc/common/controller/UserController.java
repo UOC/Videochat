f15c82c269f14b28a6cfd925fb841ad18584c3a1
Antoni Bertran <antoni@tresipunt.com>
2014-03-26 10:58:15 +0100

    Added new action of video chat (should be the default )

--- videochat/src/main/java/edu/uoc/common/controller/UserController.java
+++ videochat/src/main/java/edu/uoc/common/controller/UserController.java
@@ -1,18 +1,14 @@
 package edu.uoc.common.controller;
 
 import edu.uoc.dao.UserDao;
-import edu.uoc.lti.LTIEnvironment;
 import edu.uoc.model.User;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.ContextLoader;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -50,6 +46,30 @@ public ModelAndView handleRequestInternal(HttpServletRequest request,
 		
  
 		    
+        
+        
+    }
+
+@RequestMapping("/videochat")    
+public ModelAndView showVideochat(HttpServletRequest request,
+		HttpServletResponse response)  {
+    
+//LTIAuthenticator lti = new LTIAuthenticator();
+        
+        
+    ApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
+		ModelAndView model = new ModelAndView("videochat");
+                User user = context.getBean(User.class);
+                
+                user.setPassword("");
+                user.setUsername(user.getFullname());
+                user.setSurname(user.getFullname());
+                user.setBlocked((byte)0);
+                
+		model.addObject("user", user.getFullname());
+                userDao.save(user);
+                
+		return model;
         
         
     }
